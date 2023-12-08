4cd6e33184116a1e882ed99cab62f7b6745b8fbf
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-12-08 20:52:45 +0000

    Bot now listens for code block and can send code review to users DM on user approval.

--- bot.js
+++ bot.js
@@ -1,68 +1,83 @@
-const {Client, GatewayIntentBits} = require("discord.js");
+const {Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, ComponentType} = require("discord.js");
 require('dotenv').config()
 const {ACCESS_TOKEN_DISCORD} = process.env;
 const {ACCESS_TOKEN_OPENAI} = process.env;
 const OpenAI = require("openai");
 
-// Open API initialisation
-
 const openai = new OpenAI({
-  apiKey: ACCESS_TOKEN_OPENAI,
-});
+    apiKey: ACCESS_TOKEN_OPENAI,
+  });
 
-let log = [{ role: "system", content: "You are a friendly coding tutor" }] // Initalises conversation log for general
+let log = [{ role: "system", content: "You are a code reviewer who off short reviews on code quality" }] // Initalises conversation log for code reviewer
 
-let log2 = [{ role: "system", content: "You are a code reviewer who off short reviews on code quality" }] // Initalises conversation log for code reviewer
-
-// Inialisaes discord client interations
+// Initialises discord client interations
 const client = new Client({
     intents:[
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages
     ]
 });
+
+// Logs message to console
 client.on("ready", () =>{
     console.log("The AI bot is online"); //message when bot is online
 });
+
 client.on("messageCreate", async (message) => {
+  console.log(message);
+  if (message.author.bot) return;
 
-console.log(message);
+  // client listens for code block
+  if (message.content.startsWith('`') && message.content.endsWith('`')) {
 
-  if (message.content.startsWith("!")) {
+  // Button creation
+  const firstButton = new ButtonBuilder()
+  .setLabel("Yes")
+  .setStyle(ButtonStyle.Primary)
+  .setCustomId("yes")
 
-    await message.channel.sendTyping(); // Notifies user bot is typing
+  const secondButton = new ButtonBuilder()
+  .setLabel("No")
+  .setStyle(ButtonStyle.Danger)
+  .setCustomId("no")
 
-log.push({role: "user", content: message.content}); // pushes user response to  log
+  const buttonRow = new ActionRowBuilder().addComponents(firstButton, secondButton);
 
-// Completion feeds message log to open AI and obtains response
+  // Bot replies with button query
+  const reply = await message.reply({content: "Would you like me to review this code?", components:[buttonRow]})
+
+   const collector = reply.createMessageComponentCollector({
+    ComponentType: ComponentType.Button
+   })
+
+   // Listens to response of button query
+   collector.on('collect', async (interaction) => {
+    if (interaction.customId === "yes"){
+interaction.reply("Code review is being sent to you via DM.");
+await message.channel.sendTyping(); // Notifies user bot is typing
+    
+// Requests code review response from openAI
+log.push({role: "user", content: `please review the following code: ${message}`}) // pushes code review request to log
+
 const completion = await openai.chat.completions.create({
   messages: log,
-  model: "gpt-3.5-turbo",
-  max_tokens: 50
-});
-
-message.channel.send(completion.choices[0].message.content); // Response is logged to channel
-
-log.push({role: "system", content: completion.choices[0].message.content}) // Response pushed to log
-}
-
-// Code reviews / checks for code block by listening for indents
-if (message.content.startsWith('`') && message.content.endsWith('`')) {
-
-  await message.channel.sendTyping(); // Notifies user bot is typing
-
-log2.push({role: "user", content: `please review the following code: ${message}`}) // pushes code review request to log
-
-const completion2 = await openai.chat.completions.create({
-  messages: log2,
   model: "gpt-3.5-turbo", 
   max_tokens: 1000
 });
 
-message.reply(completion2.choices[0].message.content); //code review Response is logged to channel
+// Sent code review to DMs
+message.author.send(completion.choices[0].message.content); //code review Response is logged to channel
 
-log2.push({role: "system", content: completion2.choices[0].message.content}) // code review is logged in conversation
+log.push({role: "system", content: completion.choices[0].message.content}) // code review is logged in conversation
+
+    }
+    if (interaction.customId === "No"){
+      return;
+    }
+   })
+
 }
 
 });
